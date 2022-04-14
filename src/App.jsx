0250e53bb456748bbd58fc3d6ba5883f4cdf8400
Lotek <lotek@Loteks-MBP.home>
2022-04-14 21:40:39 +0200

    first initial

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import LoginForm from "./components/LoginForm"
+import { createContext, useState } from 'react'
+import ReactSwitch from 'react-switch'
+
+export const ThemeContext = createContext(null)
+
+function App() {
+  const [theme, setTheme] = useState("dark")
+
+  const toggleTheme = () => {
+    setTheme((curr) => (curr === "light" ? "dark" : "light"))
+  }
+
+  return (
+    <ThemeContext.Provider value={{theme, toggleTheme}}>
+    <div className="App" id={theme}>
+      <LoginForm />
+      <div className="switch" >
+      <label>{theme === "light" ? "Light Mode" : "Dark Mode" }</label>
+      <ReactSwitch 
+            onChange={toggleTheme} 
+            checked={theme === "dark"}
+            
+            />
+      </div>
+    </div>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App
